37309e7d8be55ed84ec614fc9fbe099353c2ae18
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-27 23:37:35 +0800

    added report issue button

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,6 +1,10 @@
 import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { baseOptions } from "@/lib/layout.shared";
+import { createElement } from 'react';
+import { icons } from 'lucide-react';
+
+const reportIcon = createElement(icons['MessageCircle']);
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
 	const base = baseOptions();
@@ -10,11 +14,19 @@ export default function Layout({ children }: LayoutProps<"/docs">) {
 			tree={source.pageTree}
 			{...base}
 			githubUrl="https://github.com/omeriadon/immune"
+			links={[
+				{
+					type: "icon",
+					text: "Report Issue",
+					icon: reportIcon,
+					url: "https://github.com/omeriadon/immune/issues/new"
+				}
+			]}
 			nav={{
 				...base.nav,
 				enabled: true,
 				transparentMode: "always",
-				title: "Immune"
+				title: "Immune",
 			}}
 		>
 			{children}
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,12 +1,21 @@
 import { docs } from 'fumadocs-mdx:collections/server';
 import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
   plugins: [lucideIconsPlugin()],
+  icon(icon) {
+      if (!icon) {
+        
+        return;
+      }
+      if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+    },
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
