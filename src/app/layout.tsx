192fde235aec2bdacb2c9659833595c08a216333
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-28 13:21:06 +0800

    idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 
+
 const inter = Inter({
   subsets: ['latin'],
 });
@@ -10,6 +11,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+
         <RootProvider
         search={{
         enabled: true
