a1797d478f80f534ec23a3c8478c46c7a1ce77e4
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-30 00:03:01 +0800

    colour and /docs refinements

--- src/app/(home)/docs/page.tsx
+++ src/app/(home)/docs/page.tsx
@@ -4,11 +4,23 @@ import { Baby, Power, PersonStanding, Replace } from "lucide-react";
 export default function DocsPage() {
 	return (
 		<main className="flex flex-col justify-center items-center px-4 py-16 text-center z-2">
-			<h1 className="mb-4 text-3xl font-semibold md:text-4xl">Immune.</h1>
-			<p className="text-fd-muted-foreground">
+			<h1 className="mb-8 text-3xl font-semibold md:text-4xl text-fd-primary">
+				Getting Started
+			</h1>
+			<p className="text-fd-muted-foreground mb-8">
 				The different sections of the immune system.
+				<br />
+				If you don't know where to start, begin at{" "}
+				<Link
+					href="/docs/introduction"
+					className="text-fd-primary underline underline-offset-2 decoration-2 hover:text-fd-primary/70 transition"
+				>
+					Introduction
+				</Link>
+				.
 			</p>
-			<div className="mt-8 grid gap-4 text-start w-full md:w-[80vh]">
+
+			<div className="mt-8 grid gap-6 text-start w-full md:w-[90vh]">
 				{[
 					{
 						name: "Introduction",
@@ -34,37 +46,59 @@ export default function DocsPage() {
 						icon: <PersonStanding className="size-full" />,
 						href: "/docs/immune-organs-tissues",
 					},
-				].map((item) => (
-					<Item key={item.name} href={item.href}>
-						<div className="flex gap-3 items-center mb-1">
-							<Icon>{item.icon}</Icon>
-							<h2 className="mb-2 font-medium">{item.name}</h2>
-						</div>
-						<p className="text-sm text-fd-muted-foreground">
-							{item.description}
-						</p>
-					</Item>
-				))}
+				].map((item) => {
+					return (
+						<Item key={item.name} href={item.href}>
+							<div className={`flex gap-3 items-center mb-1`}>
+								<Icon href={item.href}>{item.icon}</Icon>
+								<h2 className="mb-2 font-medium text-xl">{item.name}</h2>
+							</div>
+							<p className="text-sm text-fd-muted-foreground">
+								{item.description}
+							</p>
+						</Item>
+					);
+				})}
 			</div>
 		</main>
 	);
 }
 
-function Icon({ children }: { children: React.ReactNode }) {
+function Icon({ href, children }: { href: string; children: React.ReactNode }) {
+	const lastSlug = href.split("/").filter(Boolean).pop();
+	const colorVar = `--${lastSlug}-color`;
+	const color = `var(${colorVar})`;
 	return (
-		<div className="mb-2 size-8 rounded-lg border p-1 text-fd-muted-foreground bg-fd-muted shadow-md">
+		<div
+			className="mb-2 size-8 [&_svg]:size-full rounded-lg text-tab-color bg-(--tab-color)/10 border p-1"
+			style={
+				{
+					"--tab-color": color,
+				} as object
+			}
+		>
 			{children}
 		</div>
 	);
 }
 
-function Item(props: LinkProps & { children: React.ReactNode }) {
+interface ItemProps extends LinkProps {
+	href: string;
+	children: React.ReactNode;
+}
+
+function Item({ href, children, ...rest }: ItemProps) {
+	const lastSlug = href.split("/").filter(Boolean).pop();
+	const color = `var(--${lastSlug}-color, var(--color-fd-foreground))`;
+
 	return (
 		<Link
-			{...props}
-			className="bg-fd-card rounded-2xl border p-4 shadow-lg hover:bg-fd-popover transition-all"
+			href={href}
+			{...rest}
+			className={`bg-fd-card rounded-3xl border p-4 shadow-lg hover:bg-fd-popover transition-all`}
+			style={{ color }}
 		>
-			{props.children}
+			{children}
 		</Link>
 	);
 }
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,39 @@
 import { HomeLayout } from "fumadocs-ui/layouts/home";
-import { baseOptions } from "@/lib/layout.shared";
+import { BaseLayoutProps } from "fumadocs-ui/layouts/links";
+import { createElement } from "react";
+import { icons, ShieldPlus } from "lucide-react";
+
+const bookIcon = createElement(icons["Book"]);
+
+export function baseOptions(): BaseLayoutProps {
+	return {
+		nav: {
+			title: (
+				<>
+					<ShieldPlus
+						className="size-9 stroke-[2.5] text-fd-background"
+						fill="var(--color-fd-primary)"
+					/>
+					<span className="font-bold text-2xl text-fd-primary">Immune</span>
+				</>
+			),
+		},
+	};
+}
 
 export default function Layout({ children }: LayoutProps<"/">) {
-	return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+	return (
+		<HomeLayout
+			{...baseOptions()}
+			links={[
+				{
+					text: "Docs",
+					icon: bookIcon,
+					url: "/docs",
+				},
+			]}
+		>
+			{children}
+		</HomeLayout>
+	);
 }
