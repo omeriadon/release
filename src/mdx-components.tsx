7202327a24e4cce70fb9f03ef1f86963a695a9aa
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-02-15 17:53:53 +0800

    added highlit

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,18 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
+
+const Highlight = (props: React.PropsWithChildren<{ className?: string }>) => (
+	<span
+		{...props}
+		style={{
+			backgroundColor: "rgba(16, 179, 171, 0.7)",
+			padding: "0.1rem 0.2rem",
+			borderRadius: "5px",
+		}}
+	/>
+);
+
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
@@ -8,6 +20,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
 		h2: (props) => <h2 {...props} className="text-fd-primary" />,
 		h3: (props) => <h3 {...props} className="text-fd-primary" />,
 		h4: (props) => <h4 {...props} className="text-fd-primary" />,
+		Highlight,
 		...components,
 	};
 }
